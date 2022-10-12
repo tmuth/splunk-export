1980c22d151b9d31e82ecbd821eff858d83714d4
Tyler Muth <tylerdmuth-git@yahoo.com>
2022-10-12 11:10:33 -0400

    {Version number}

--- splunk-export-parallel.py
+++ splunk-export-parallel.py
@@ -17,7 +17,7 @@ from splunk_hec import splunk_hec
 __author__ = "Tyler Muth"
 __source__ = "https://github.com/tmuth/splunk-export"
 __license__ = "MIT"
-__version__ = "20221012_110931"
+__version__ = "20221012_111033"
 
 
 if len(sys.argv) < 2:
